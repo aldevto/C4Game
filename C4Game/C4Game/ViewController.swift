a4d957d155bed57d52eb93f17e95ac6ed44e4da2
aldevto <aldevto@gmail.com>
2022-01-10 16:15:24 +0100

    Create a Xcode Project

---
+++ C4Game/C4Game/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  C4Game
+//
+//  Created by Alberto Martinez on 10/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ C4Game/C4GameUITests/C4GameUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  C4GameUITestsLaunchTests.swift
+//  C4GameUITests
+//
+//  Created by Alberto Martinez on 10/1/22.
+//
+
+import XCTest
+
+class C4GameUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
